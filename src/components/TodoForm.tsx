832f096e58c000b56885eb50aff8faf51d9514a8
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-07-15 16:25:37 +0900

    TDD를 통한 TodoForm.tsx 설계

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,6 +1,26 @@
-export const TodoForm = () => (
-    <form>
-        <input placeholder="할 일을 입력하세요" />
-        <button type="submit">등록하기</button>
-    </form>
-);
+import React, {ChangeEvent, FormEvent, useState} from 'react';
+
+interface TodoFormProps {
+    onInsert: (value: string) => void;
+}
+
+export const TodoForm = ({onInsert}: TodoFormProps) => {
+    const [value, setValue] = useState('');
+
+    const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value);
+    };
+
+    const onSubmitTodo = (e: FormEvent<HTMLFormElement>) => {
+        onInsert(value);
+        e.preventDefault();
+        setValue('');
+    };
+
+    return (
+        <form onSubmit={onSubmitTodo}>
+            <input placeholder="할 일을 입력하세요" onChange={handleInput} value={value}/>
+            <button type="submit">등록하기</button>
+        </form>
+    );
+};
--- src/tests/TodoForm.test.tsx
+++ src/tests/TodoForm.test.tsx
@@ -1,13 +1,45 @@
-import { render, screen } from '@testing-library/react';
-
-import { TodoForm } from '@/components/TodoForm';
+import { render, screen, fireEvent } from "@testing-library/react";
+import { TodoForm } from "@components/TodoForm"
 
 describe('<TodoForm />', () => {
-    it('has input and a button', () => {
-        render(<TodoForm />);
+    const setup = () => {
+        const onInsert = jest.fn();
+        const utils = render(<TodoForm onInsert={onInsert} />);
         const input = screen.getByPlaceholderText('할 일을 입력하세요');
         const button = screen.getByText('등록하기');
+        return {
+            ...utils,
+            input,
+            button,
+            onInsert,
+        };
+    };
+
+    it('has input and a button', () => {
+        const { input, button } = setup();
         expect(input).toBeInTheDocument();
         expect(button).toBeInTheDocument();
+    });
+
+    it('changes input', () => {
+        const { input } = setup();
+        fireEvent.change(input, {
+            target: {
+                value: 'TDD-TODO',
+            },
+        });
+        expect(input).toHaveAttribute('value', 'TDD-TODO');
+    });
+
+    it('calls onInsert and clears input', () => {
+        const { input, button, onInsert } = setup();
+        fireEvent.change(input, {
+            target: {
+                value: 'TDD-TODO',
+            },
+        });
+        fireEvent.click(button);
+        expect(onInsert).toBeCalledWith('TDD-TODO');
+        expect(input).toHaveAttribute('value', '');
     });
 });
